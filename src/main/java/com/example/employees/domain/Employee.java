7b4bbe481ca289b96982b4a578f651296bbe3425
Jonathan Sánchez <jsanchezorellana@gmail.com>
2018-04-01 21:18:52 -0300

    Added Spring Annotations

--- src/main/java/com/example/employees/domain/Employee.java
+++ src/main/java/com/example/employees/domain/Employee.java
@@ -1,14 +1,26 @@
 package com.example.employees.domain;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "employee")
 public class Employee {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
     private long id;
 
+    @Column(name = "name")
     private String name;
 
+    @Column(name = "last_name")
     private String lastName;
 
+    @Column(name = "salary")
     private double salary;
+
+    public Employee() {}
 
     public Employee(long id, String name, String lastName, double salary) {
         this.id       = id;
